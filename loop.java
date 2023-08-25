84189517ec5b2c3e9856683b495f2bfee2a63239
Abhicode10 <myselfabhishekyadav10@gmail.com>
2023-08-25 20:33:43 +0530

    Add Initial Files

---
+++ loop.java
@@ -0,0 +1,27 @@
+// Print the first n factorial numbers
+
+import java.util.Scanner;
+
+public class loopexxxxx {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Enter a given number");
+
+        int n = SC.nextInt();
+        int num = n;
+
+        int fact = 1;
+        for(int i = 1; i<=n; i++){
+            fact = fact * i;
+            System.out.println("The factorial of " + i  +" : " +fact);
+
+        }
+        
+      
+      System.out.println("The factorial of " +num +" is " +fact);
+        
+      
+
+    }
+    
+}
